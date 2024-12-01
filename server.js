d27624bc247f20470f1a00a5983e67d7653cfa6f
Sarlin-22 <arthur14042005@gmail.com>
2024-12-01 20:17:55 -0300

    ultimo

--- server.js
+++ server.js
@@ -108,6 +108,59 @@ fastify.delete('/pedido/:id', async (req, reply) => {
     }
 });
 
+fastify.put('/pedido/quantidade/:id', async (req, reply) => {
+    const { id } = req.params;
+    const { quantidade } = req.body;
+
+    try {
+        const [pedido] = await db.query('SELECT * FROM pedido WHERE id = ?', [id]);
+
+        if (pedido.length === 0) {
+            return reply.code(404).send({ message: 'Pedido não encontrado' });
+        }
+
+        const produto_id = pedido[0].produto_id;
+        const quantidadeAnterior = pedido[0].quantidade;
+
+        // Busca o produto na API
+        const productResponse = await axios.get(
+            `https://av3-arquitetura-de-projetos-production.up.railway.app/api/products/${produto_id}`
+        );
+
+        if (!productResponse.data) {
+            return reply.code(404).send({ message: 'Produto não encontrado na API' });
+        }
+
+        const produto = productResponse.data;
+
+        // Verificar se a estoque suficiente para a quantidade
+        if (produto.stock < (quantidade - quantidadeAnterior)) {
+            return reply.code(400).send({ message: 'Estoque insuficiente para atualizar a quantidade' });
+        }
+
+        // Atualiza a quantidade e o valor_total
+        const valorTotal = produto.price * quantidade;
+        const [result] = await db.execute(
+            'UPDATE pedido SET quantidade = ?, valor_total = ? WHERE id = ?',
+            [quantidade, valorTotal, id]
+        );
+
+        // Atualizar o estoque na API
+        const quantidadeAlterada = quantidade - quantidadeAnterior;
+        await axios.put(
+            `https://av3-arquitetura-de-projetos-production.up.railway.app/api/products/${produto_id}/stock?quantity=${quantidadeAlterada}`
+        );
+
+        reply.send({
+            message: 'Quantidade atualizada com sucesso e estoque ajustado',
+            affectedRows: result.affectedRows
+        });
+    } catch (error) {
+        console.error(error);
+        reply.code(500).send({ message: 'Erro ao atualizar pedido', error: error.message });
+    }
+});
+
 // Iniciando o servidor
 const start = async () => {
     try {
